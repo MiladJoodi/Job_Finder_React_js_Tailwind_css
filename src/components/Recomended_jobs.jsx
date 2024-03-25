2226968e9007c2d74715c4a89cb5ffed89bfe779
Milad Joodi <winbeta.com@gmail.com>
2024-03-25 18:29:47 +0330

    up

--- src/components/Recomended_jobs.jsx
+++ src/components/Recomended_jobs.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
 import Categories from './Categories'
 
+const data = [
+    {
+        id: 1,
+        image: "/bus_icon.webp",
+        title: "Automotive",
+        opening: 3
+    },
+
+    {
+        id: 2,
+        image: "/health.png",
+        title: "Health",
+        opening: 3
+    },
+
+    {
+        id: 3,
+        image: "/arch.png",
+        title: "Architect",
+        opening: 3
+    },
+
+    {
+        id: 4,
+        image: "/fit.png",
+        title: "Fitness",
+        opening: 3
+    },
+
+    {
+        id: 5,
+        image: "/ind.png",
+        title: "Industry",
+        opening: 3
+    },
+]
+
 function Recomended_jobs() {
     return (
         <div className='bg-[#f5f6fc]'>
@@ -10,23 +47,29 @@ function Recomended_jobs() {
                     <h2 className='xl:text-5xl lg:text-3xl text-2xl font-semibold'>
                         Recomended jobs
                     </h2>
-                    <span className='md:flex gap-x-4'>
+                    <div className='md:flex gap-x-4'>
                         <button className='rounded-full md:my-0 my-4 bg-indigo-700 text-lg text-white font-bold px-8 py-3 ouutline-none border-none hoverBtn'>Latest Job</button>
                         <button className='rounded-full bg-transparent border border-solid border-[#e2e4e7] text-lg text-black font-bold px-8 py-3 outline-none shadow hoverBtn'>Premium Job</button>
-                    </span>
+                    </div>
                 </div>
 
                 <p className='text-2xl mt-4 font-light'>Explore suggested job searches</p>
 
-                <div>
-                    <div>
-                        <div>
-                            <h2>Job Categories</h2>
+                <div className='xl:flex gap-x-8 mt-16'>
+                    <div className='xl:w-2/5'>
+                        <div className='bg-white rounded-[10px] py-8 pl-6 mb-4'>
+                            <h2 className='text-4xl font-semibold'>Job Categories</h2>
                         </div>
-                        <div>
-                            <Categories image="/bus_icon.webp" opening={3} title="Automotive" />
+                        <div className='flex flex-col gap-y-4'>
+                            {data.map((cat, index) => (
+                                <Categories key={index} image={cat.image} opening={cat.opening} title={cat.title} />
+                            ))}
                         </div>
                     </div>
+                </div>
+
+                <div>
+                    ss
                 </div>
 
             </div>
---
+++ src/components/Recommended.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Recommended({time, type, title, amount, country, job, bgColor, color}) {
+  return (
+    <div className=''>
+        <div>
+            <span>
+                <span>
+                    <button>
+                        <GrAnnounce />
+                    </button>
+                </span>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Recommended
